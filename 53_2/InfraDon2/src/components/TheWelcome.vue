4def929a82ef30ac3f928d3f08090a90ddcf0311
Unseile <lea.haeberli03@gmail.com>
2025-10-02 15:42:59 +0200

    init Vue

---
+++ 53_2/InfraDon2/src/components/TheWelcome.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+const counter = ref(20)
+
+const increment = () => {
+  counter.value++
+}
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <button @click="increment">Increment</button>
+  <p style="color: white; font-size: 50px;">{{ counter }}</p>
+</template>
